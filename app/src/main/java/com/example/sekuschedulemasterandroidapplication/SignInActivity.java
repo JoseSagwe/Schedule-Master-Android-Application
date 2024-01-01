37ad8d58350f7878851a23950e30b75b4e8b2ae8
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-01 20:16:20 +0300

    activities xml

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
@@ -2,7 +2,9 @@ package com.example.sekuschedulemasterandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+    //End of On Create Activity
+
+    public void goToHome(View view){
+        Intent intent = new Intent (SignInActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
