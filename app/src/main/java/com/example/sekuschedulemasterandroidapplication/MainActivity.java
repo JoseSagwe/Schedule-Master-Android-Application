75d6a4290db6c60ffeb61be1fbe9eeae35031528
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-01 15:56:07 +0300

    main activity

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.sekuschedulemasterandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sign_in, sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //End of On Create Method
+
+    public void goToSignUp(View view){
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+    //End of Sign Up Activity
+
+    public void goToSignIn(View view){
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+    //End of Sign In Activity
+
+
+
 }
