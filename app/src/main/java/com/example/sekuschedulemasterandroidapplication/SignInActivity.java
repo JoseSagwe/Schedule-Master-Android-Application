fdc107e84294abb2e676c382542a6cee6b4cd9c4
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-05 15:06:31 +0300

    Sign In btn

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    Button sign_in_btn;
+    EditText et_email, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Hook Edit Text Fields
+        et_email = findViewById(R.id.email);
+        et_password = findViewById(R.id.password);
+
+        //Hook Button
+        sign_in_btn = findViewById(R.id.sign_in_btn);
+
     }
     //End of On Create Activity
 
