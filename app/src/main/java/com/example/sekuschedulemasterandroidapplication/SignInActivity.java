24c13127558c7d4fb0f6dd11fca74b0adb299685
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-08 23:20:40 +0300

    setting JSON Request Object

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/SignInActivity.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.sekuschedulemasterandroidapplication.helpers.StringHelper;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -61,6 +67,19 @@ public class SignInActivity extends AppCompatActivity {
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("email", et_email.getText().toString());
         params.put("password", et_password.getText().toString());
+
+        //Set Request Object
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                
+            }
+        });  // End of Set Request Object
 
     }
 
