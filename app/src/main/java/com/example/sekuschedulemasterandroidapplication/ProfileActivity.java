32353b37aa0e8e9564528ef8a2b3dc1e9bb7c61e
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-09 10:23:31 +0300

    sign out user profile activity

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.sekuschedulemasterandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,7 +53,9 @@ public class ProfileActivity extends AppCompatActivity {
         tv_email.setText(null);
 
         //Return User back to home page
-        
+        Intent goToHome = new Intent(ProfileActivity.this, MainActivity.class);
+        startActivity(goToHome);
+        finish();
     }
 
 }
