c4c1852c03257ab0a8e6509a752473e1853bdab4
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-09 09:48:31 +0300

    hook fields in SignIn activity

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
@@ -14,7 +14,20 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Hook Text View
+        first_name = findViewById(R.id.first_name);
+        last_name = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+
+        //Hook Sign Out Button
+        sign_out_btn = findViewById(R.id.sign_out_btn);
+
+        //Set onclick Listener
+
+
     }
+
 
     public void signUserOut(){
 
