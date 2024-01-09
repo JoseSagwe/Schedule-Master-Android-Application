a402ae012dec5fe64999c23cfb2ce63a8e7b11de
JoseSagwe <Josephsagwe421@gmail.com>
2024-01-09 09:38:00 +0300

    sign out profile activity

--- app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
+++ app/src/main/java/com/example/sekuschedulemasterandroidapplication/ProfileActivity.java
@@ -3,12 +3,21 @@ package com.example.sekuschedulemasterandroidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TextView first_name, last_name, email;
+    Button sign_out_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    public void signUserOut(){
+
+    }
+
 }
